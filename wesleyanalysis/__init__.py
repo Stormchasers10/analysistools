84263122e4e363eea798a07e07684456be38840e
Wesley Ishida <wesley_ishida@berkeley.edu>
2026-02-05 16:00:41 -0800

    Updated init and core files to include theory_fobs_vs_fs plot

--- wesleyanalysis/__init__.py
+++ wesleyanalysis/__init__.py
@@ -1,2 +1,8 @@
-<<<<<<< HEAD
-from .core import capture_data, voltage_spectrum, power_spectrum
+from .core import (
+    capture_data,
+    voltage_spectrum,
+    power_spectrum,
+    alias_peak,
+    theory_fobs_vs_fs,
+    logspace_fs,
+)
--- wesleyanalysis/core.py
+++ wesleyanalysis/core.py
@@ -60,3 +60,49 @@ def power_spectrum(data):
     fft_data = np.fft.fft(data)
     power = np.abs(fft_data**2)
     return power
+
+
+###### This is the start of the theory plot of f_obs vs f_samp plot ######
+
+import numpy as np
+
+def alias_peak(f0, fs):
+    """
+    Return aliased frequency in the FIRST Nyquist zone [0, fs/2].
+    f0 and fs must be in the same units.
+    Works with scalars or numpy arrays.
+    """
+    f0 = np.asarray(f0, dtype=float)
+    fs = np.asarray(fs, dtype=float)
+
+    r = np.mod(f0, fs)                 # in [0, fs)
+    return np.where(r <= fs/2, r, fs - r)
+
+def theory_fobs_vs_fs(f0, fs_vals):
+    """
+    Returns (fs_vals, fobs_vals) for the aliasing theory curve.
+    No plotting. Units pass through unchanged.
+
+    Parameters
+    ----------
+    f0 : float
+        True signal frequency (same units as fs_vals)
+    fs_vals : array-like
+        Sampling frequencies (same units as f0)
+
+    Returns
+    -------
+    fs_vals : np.ndarray
+    fobs_vals : np.ndarray
+    """
+    fs_vals = np.asarray(fs_vals, dtype=float)
+    fobs_vals = alias_peak(f0, fs_vals)
+    return fs_vals, fobs_vals
+
+def logspace_fs(fs_max, fs_min, npts=400):
+    """
+    Convenience: returns log-spaced sampling frequencies.
+    """
+    return np.logspace(np.log10(fs_max), np.log10(fs_min), int(npts))
+
+    ###### This is the end of the theory plot of f_obs vs f_samp plot ######
